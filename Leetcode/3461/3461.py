90a81b38aa9f514ac638cad8952509d2173d7848
Pradhansumit <pradhansumit026@gmail.com>
2025-10-23 07:11:55 +0530

    LC3461 O(n2)

---
+++ Leetcode/3461/3461.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hasSameDigits(self, s: str) -> bool:
+        while len(s) > 2:
+            new_str = ""
+            for i in range(len(s) - 1):
+                new_str += str((int(s[i]) + int(s[i + 1])) % 10)
+            s = new_str
+
+        return True if s[0] == s[1] else False
+
+
+sol = Solution()
+print(sol.hasSameDigits("3902"))
+print(sol.hasSameDigits("34789"))
